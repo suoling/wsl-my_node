8e90000490385f33250baf9ec0ac58014b3cbe5f
suoling <wangsuoling@163.com>
2019-09-19 14:58:30 +0800

    update

--- getBaidu.js
+++ getBaidu.js
@@ -1,15 +1,57 @@
 
 var http=require('http');
 //get 请求外网
-http.get('http://www.baidu.com',function(res){
-	var html='';
-	var len = 0
-	res.on('data',function(data){
-		html+=data;
-		len +=1;
-	});
-	res.on('end',function(){
-		// console.info(html);
-	});
+// http.get('http://www.baidu.com',function(res){
+// 	var html='';
+// 	var len = 0
+// 	res.on('data',function(data){
+// 		html+=data;
+// 		len +=1;
+// 	});
+// 	res.on('end',function(){
+// 		// console.info(html);
+// 	});
+// 	console.log(res)
+// });
+
+// promise
+// let getBaidu = new Promise((resolve, reject) => {
+// 	http.get('http://www.baidu.com',function(res){
+// 		var html='';
+// 		var len = 0
+// 		res.on('data',function(data){
+// 			html+=data;
+// 			len +=1;
+// 		});
+// 		res.on('end',function(){
+// 			resolve(html)
+// 		});
+// 	});
+// })
+// getBaidu.then(res => {
+// 	console.log(res)
+// })
+
+// async await
+function getBaidu () {
+	return new Promise((resolve, reject) => {
+		http.get('http://www.baidu.com',function(res){
+			var html='';
+			var len = 0
+			res.on('data',function(data){
+				html+=data;
+				len +=1;
+			});
+			res.on('end',function(){
+				resolve(html)
+			});
+		});
+	})
+}
+
+async function fn () {
+	const res = await getBaidu()
 	console.log(res)
-});
+}
+
+fn()
